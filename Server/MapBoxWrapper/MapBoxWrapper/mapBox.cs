d9f099a2bea2f7ff7f432a8553591ae2a184c66d
Julia <jfranck@ufl.edu>
2019-10-26 09:28:17 -0400

    Created RouteGeneration Class and added and renamed mapBox.cs

--- Server/MapBoxWrapper/MapBoxWrapper/mapBox.cs
+++ Server/MapBoxWrapper/MapBoxWrapper/mapBox.cs
@@ -9,22 +9,14 @@ using Newtonsoft.Json.Linq;
 namespace MapBoxWrapper {
     public class mapBox {
 
-        
         //Longitude, latitude
 
         private const string walkingURL = "https://api.mapbox.com/directions/v5/mapbox/walking/";
         private double distance { set; get; }
         private Tuple<double, double> startPoint { set; get; }
         private Tuple<double, double> endPoint { set; get; }
-
         private string accessToken { get; }
-        
         private static HttpClient webber = new HttpClient();
-
-        
-        
-        
-
 
         public mapBox(double distance, Tuple<double, double> startPoint, Tuple<double, double> endPoint) {
             this.distance = distance;
@@ -35,60 +27,46 @@ namespace MapBoxWrapper {
 
         }
         
-        
-
         public void firstStep(List<Tuple<double, double>> listOfPoints) {
-            
-            
-            
+
             
         }
-
 
         public async Task<double> getRealDistance() {
 
-
             string fullURL = walkingURL + createPathOfPoints(new List<Tuple<double, double>>{startPoint, endPoint});
-
-            fullURL += $".json?access_token={accessToken}";
-            
+            fullURL += $".json?access_token={accessToken}";          
             var response = await webber.GetAsync(fullURL);
-
             var content = await response.Content.ReadAsStringAsync();
-
             JObject jsonData = JsonConvert.DeserializeObject<JObject>(content);
-
             double stringCheck = (double)jsonData["code"];
-
             return stringCheck;
         }
 
-
         public string createPathOfPoints(List<Tuple<double, double>> points) {
-            
-            
             
             string fullURL = "";
 
-            for (int i = 0; i < points.Count - 1; i++) {
-                
+            for (int i = 0; i < points.Count - 1; i++) {  
                 fullURL += startPoint.Item1 + "%2C" + startPoint.Item2 + "%3B";
-
             }
-
             fullURL += points[points.Count - 1].Item1 + "%2C" + points[points.Count - 1].Item2;
-
-
-
             return fullURL;
-
-
 
         }
 
-
-
-
+        public Tuple<double, double> getStartPoint()
+        {
+            return startPoint;
+        }
+        public Tuple<double, double> getEndPoint()
+        {
+            return endPoint;
+        }
+        public double getDesiredDistance()
+        {
+            return distance;
+        }
 
     }
 }
